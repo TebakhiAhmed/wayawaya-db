d5f9d8836973b2e818c400c2fdee0acd170957dc
C5380672 <ahmed.tebakhi@sap.com>
2024-09-24 15:50:52 +0200

    update the db

--- db.js
+++ db.js
@@ -1,22 +1,26 @@
 // db.js
-import mysql from "mysql2";
+import mysql from "mysql2/promise"; // Use promise for better async support
 
-const db = mysql.createConnection({
+const pool = mysql.createPool({
   host: 'srv915.hstgr.io',
   user: 'u222886794_admin',
   password: '_9VpyXfH2L6hiZH',
-  database: 'u222886794_wayawayadb'
+  database: 'u222886794_wayawayadb',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  keepAliveInitialDelay: 10000, // 10 seconds initial delay
+  enableKeepAlive: true // Enable keep-alive
 });
 
-
-db.connect((err) => {
-  if (err) {
+// Optional: Test connection to ensure the pool is working
+pool.getConnection()
+  .then(connection => {
+    console.log('Connected to the database as ID', connection.threadId);
+    connection.release(); // Release the connection back to the pool
+  })
+  .catch(err => {
     console.error('Error connecting to the database:', err.stack);
-    return;
-  }
-  console.log('Connected to the database as ID', db.threadId);
-});
+  });
 
-
-
-export default db;
+export default pool;
