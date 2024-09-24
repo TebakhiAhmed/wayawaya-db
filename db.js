f07459db1f027bac27d2f0b102efd269401a7026
C5380672 <ahmed.tebakhi@sap.com>
2024-09-24 14:46:00 +0200

    add connet the db

--- db.js
+++ db.js
@@ -1,14 +1,14 @@
-const mysql = require('mysql');
+// db.js
+import mysql from "mysql2";
 
-// Create a connection to the database
 const db = mysql.createConnection({
-  host: 'srv915.hstgr.io',       // Your database host
-  user: 'u222886794_admin',      // Your database username
-  password: '_9VpyXfH2L6hiZH',   // Your database password
-  database: 'u222886794_wayawayadb'  // Your database name
+  host: 'srv915.hstgr.io',
+  user: 'u222886794_admin',
+  password: '_9VpyXfH2L6hiZH',
+  database: 'u222886794_wayawayadb'
 });
 
-// Connect to the database
+
 db.connect((err) => {
   if (err) {
     console.error('Error connecting to the database:', err.stack);
@@ -17,4 +17,6 @@ db.connect((err) => {
   console.log('Connected to the database as ID', db.threadId);
 });
 
-export default db;  // Use export default if using ES modules
+
+
+export default db;
