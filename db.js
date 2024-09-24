f9451d56e87bc13593a5d41f5c3561c831d5e377
C5380672 <ahmed.tebakhi@sap.com>
2024-09-24 14:30:00 +0200

    update the db

--- db.js
+++ db.js
@@ -1,14 +1,18 @@
-// db.js
-import mysql from "mysql2";
-import dotenv from 'dotenv';
+const mysql = require('mysql');
 
-dotenv.config();
-
+// Create a connection to the database
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  host: 'srv915.hstgr.io',       // Your database host
+  user: 'u222886794_admin',      // Your database username
+  password: '_9VpyXfH2L6hiZH',   // Your database password
+  database: 'u222886794_wayawayadb'  // Your database name
 });
 
-export default db;
+// Connect to the database
+db.connect((err) => {
+  if (err) {
+    console.error('Error connecting to the database:', err.stack);
+    return;
+  }
+  console.log('Connected to the database as ID', db.threadId);
+});
